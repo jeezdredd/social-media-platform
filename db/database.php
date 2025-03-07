4de7f7158153d7bef43f697678be22d97a900d87
Sevastyan <6632768420@my.lerna.kz>
2025-03-07 12:45:47 +0500

    changed database.php

--- db/database.php
+++ db/database.php
@@ -1,12 +1,12 @@
 <?php
-$host = 'localhost';
-$dbname = 'webapp';
-$username = 'root'; // или другой пользователь
-$password = '';
+$host = 'localhost'; //host of the database (in our case local database MariaDB via XAMMP)
+$dbname = 'webapp'; //name of the database
+$username = 'root'; //username with the access to database
+$password = ''; //password of the user
 
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $dbname); //establishing connection to the database
 
 if ($conn->connect_error) {
-    die("Ошибка подключения: " . $conn->connect_error);
+    die("Database connection error: " . $conn->connect_error); //in case of connection faults, show the error messsage
 }
 ?>
